443ff4f9b3dba3d57b1963d113fdf582743f4234
leechulseung <gelos@likelion.org>
2017-07-15 21:10:00 +0900

    profile빼고 나머지 시안 작업 완료

--- group/urls.py
+++ group/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     url(r'^$', views.group_index , name="group_index"),
     url(r'^make/$', views.group_make, name="group_make"),
+    url(r'^group_book/$', views.group_book, name='group_book'),
 ]
